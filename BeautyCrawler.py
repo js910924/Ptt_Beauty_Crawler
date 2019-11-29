fbeb0e2172b099a76f4d41021914004a1a032b0a
js910924 <js910924@gmail.com>
2019-11-29 16:48:55 +0800

    Add flag feature

--- BeautyCrawler.py
+++ BeautyCrawler.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import os
+import sys
 import threading
+from datetime import datetime, timedelta
 
 class BeautyCrawler:
 	def __init__(self, input_date):
@@ -12,14 +14,21 @@ class BeautyCrawler:
 
 		# Set path where you want to store your pictures
 		# default is your current path
-		self.path = os.getcwd() + '/' + '.'.join(self.input_date)
-		os.mkdir(self.path)
+		self.path = os.getcwd() + '\\' + '.'.join(self.input_date)
+		if not os.path.exists(self.path):
+			os.mkdir(self.path)
  
 	def getSoup(self, url):
-	    response = requests.get(url)
-	    soup = bs(response.text, 'html.parser')
-
-	    return soup
+		payload = {
+			"from": "https://www.ptt.cc/bbs/Beauty/index.html",
+			"yes": "yes"
+		}
+		rs = requests.session()
+		res = rs.post("https://www.ptt.cc/ask/over18?from=%2Fbbs%2FBeauty%2Findex.html", data=payload)
+		res = rs.get(url)
+		soup = bs(res.text, 'html.parser')
+		
+		return soup
 
 	def previousPage(self, current_url):
 	    previous = self.soup.findAll('a', 'btn wide')[1].get('href')
@@ -28,24 +37,26 @@ class BeautyCrawler:
 	    return current_url
 
 	def getArticles(self, input_date):
-	    articles = self.soup.findAll('div', 'r-ent')
-	    NOT_EXIST = bs('<a href= "">本文已被刪除</a>', 'html.parser').a
-	    tmp = [article for article in articles]
-	    tmp.reverse()
-	    
-	    for article in tmp:
-	        meta = article.find('div', 'title').find('a') or NOT_EXIST
-	        title = meta.get_text()
-	        link = meta.get('href')
-	        self.date = article.find('div', 'date').get_text().strip().split('/')
-
-	        if link != '' and self.date == input_date:
-	            path = self.path + ('/' + title)
-	            if not os.path.isdir(path):
-	                os.mkdir(path)
+		articles = self.soup.findAll('div', 'r-ent')
+		NOT_EXIST = bs('<a href= "">本文已被刪除</a>', 'html.parser').a
+		tmp = [article for article in articles]
+		tmp.reverse()
 		
-	            t = threading.Thread(target = self.getPic, args = (link, path))
-	            t.start()
+		for article in tmp:
+			meta = article.find('div', 'title').find('a') or NOT_EXIST
+			title = meta.get_text()
+			link = meta.get('href')
+			self.date = article.find('div', 'date').get_text().strip().split('/')
+			if link != '' and self.date == input_date:
+				path = self.path + ('\\' + title)
+				if not os.path.isdir(path):
+					try:
+						os.mkdir(path)
+					except NotADirectoryError:
+						return
+				
+				t = threading.Thread(target = self.getPic, args = (link, path))
+				t.start()
 
 	def getPic(self, link, path):
 	    name = 0   # 圖檔取名用
@@ -69,18 +80,28 @@ class BeautyCrawler:
 
 	def run(self):
 		page = 1
+		now = datetime.now()
+		input_time = datetime.strptime("{0}-{1}-{2}".format(now.year, int(self.input_date[0]), int(self.input_date[1])), "%Y-%m-%d")
 
 		while True:
 			print('Page [%d]' % page)
 			page += 1
 			self.soup = self.getSoup(self.url)
 			self.getArticles(self.input_date)
-			if int(self.date[0]) < int(self.input_date[0]) or (self.date[0] == self.input_date[0] and int(self.date[1]) < int(self.input_date[1])):
+			current_time = datetime.strptime("{0}-{1}-{2}".format(now.year, int(self.date[0]), int(self.date[1])), "%Y-%m-%d")
+			if input_time > current_time:
 				print('---------------------Mission Complete---------------------')
 				break
 			self.url = self.previousPage(self.url)
 
 if __name__ == '__main__':
-	input_date = input('請輸入欲搜尋之日期 ： ')
+	if len(sys.argv) != 2:
+		now = datetime.now()
+		input_date = now.strftime("%m/%d")
+		print("Download all image today: %s" % input_date)
+	else:
+		print("Download all image on %s" % sys.argv[1])
+		input_date = sys.argv[1]
+
 	Crawler = BeautyCrawler(input_date)
 	Crawler.run()
--- README.md
+++ README.md
@@ -6,24 +6,21 @@ This implementation is for learing crawler and python programming skill.
 2019.06.18
 
 > Use multithreading to download pictures and made some change to improve efficiency.
->
 > Set default path where BeautyCrawler.py is.
 
 ## Instructions
-1. set path in function getArticles
-![](https://i.imgur.com/7kovr3w.png)
-2. execute Python file
-3. input date which you want to download (ex. 5/27) (m/dd)
-4. wait until it shows 'Mission Complete'
+install bs4 & requests before execute Crawler
+```shell
+pip install bs4
+pip install requests
+```
 
-## Screenshot
-input date (ex. 5/27)
-![](https://i.imgur.com/M3m4M4S.png)
-start downloading
-![](https://i.imgur.com/zK9hs0X.png)
-wait until it shows Mission Complete
-![](https://i.imgur.com/W7g9lrh.png)
-during the process, it will create a folder named by your input date in where you set your path 
-![](https://i.imgur.com/QvuYtyw.png)
-in this folder, it will create folders named by article's name, and inside the folders are the pictures you downloaded.
-![](https://i.imgur.com/xXbQQAs.png)
+### Download today beauty board pictures
+```shell
+python3 BeautyCrawler.py
+```
+
+### Download the specified data on beauty board pictrues
+```shell
+python3 BeautyCrawler.py 11/5
+```
